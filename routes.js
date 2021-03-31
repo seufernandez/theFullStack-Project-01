e0e0ee61a38e17d34a18707fb5bc192199711759
seufernandez <fernandez.g1337@gmail.com>
2021-03-30 15:45:03 -0900

    g

--- routes.js
+++ routes.js
@@ -9,6 +9,14 @@ routes.get('/captains', function(req, res) {
     return res.render('captains/index')
 })
 
+routes.get('/captains/create', function(req, res) {
+    return res.render('captains/create')
+})
+
+routes.post("/captains", function(req, res) {
+    return res.send("recebido")
+})
+
 routes.get('/crew', function(req, res) {
     return res.send('crew')
 })
